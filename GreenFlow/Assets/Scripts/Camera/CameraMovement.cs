a114ccbd3cd8d33ec557aa9773e052749a29e3f6
meakinl2 <lewismeakin1614@gmail.com>
2025-02-17 20:09:14 +0000

    17/02/25 Camera Movement Added

---
+++ GreenFlow/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private int moveSpeedMultiplier = 1;
+    [SerializeField]
+    private float zoomStep = 1, minCameraSize = 1, maxCameraSize = 10;
+    private Vector3 dragOrigin;
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        PanCamera();
+        ZoomCamera();
+    }
+
+    private void PanCamera() 
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 cursorDifference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+            cam.transform.position += cursorDifference * moveSpeedMultiplier;   
+        }
+    }
+
+
+    public void ZoomCamera() 
+    {
+        float newCameraSize = 0;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f )
+        {
+            newCameraSize = cam.orthographicSize + zoomStep;
+            cam.orthographicSize = Mathf.Clamp(newCameraSize, minCameraSize, maxCameraSize);
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f )
+        {
+            newCameraSize = cam.orthographicSize - zoomStep;
+            cam.orthographicSize = Mathf.Clamp(newCameraSize, minCameraSize, maxCameraSize);
+        }
+  
+    }
+
+}
